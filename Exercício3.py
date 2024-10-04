cc8c3d82c1fc73b7baeca63d136d3da2cb81ac1a
BRCMOURA <brcmoura@gmail.com>
2024-10-04 20:52:46 -0300

    Add files via upload

---
+++ Exercício3.py
@@ -0,0 +1,8 @@
+#Multiplicaçao de dois números
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número:"))
+
+multiplicacao = (n1 * n2)
+
+print("O resultado da multiplicação é: ", multiplicacao)
